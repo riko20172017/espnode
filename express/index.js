e9e5877dbdb91b7f554243f086f3a03832b9e537
anz <g_anz@mail.ru>
2025-04-08 23:23:49 +0300

    add buttons

--- express/index.js
+++ express/index.js
@@ -1,4 +1,28 @@
-const express = require('express'); // подключаем фреймворк Express (модуль)
+const express = require("express"); // подключаем фреймворк Express (модуль)
+const mqtt = require("mqtt");
+
+const protocol = "mqtt";
+const host = "192.168.0.13";
+const portMqtt = "1883";
+const clientId = `mqtt_${Math.random().toString(16).slice(3)}`;
+
+const connectUrl = `${protocol}://${host}:${portMqtt}`;
+
+const client = mqtt.connect(connectUrl, {
+  clientId,
+  clean: true,
+  connectTimeout: 4000,
+  username: "",
+  password: "",
+  reconnectPeriod: 1000,
+});
+
+client.on("connect", () => {
+  console.log("connected");
+});
+client.on("error", (error) => {
+  console.error("connection failed", error);
+});
 
 const app = express(); // создаем экземпляр приложения
 const router = express.Router(); // создаем экземпляр роутера
@@ -8,19 +32,29 @@ const port = 8080; // записываем порт сервера
 
 // выводим в консоль HTTP METHOD при каждом запросе
 router.use(function (req, res, next) {
-    console.log('/' + req.method);
-    next();
+  console.log("/" + req.method);
+  next();
 });
 
 // отвечаем на запрос главной страницы файлом index.html
-router.get('/', function (req, res) {
-    res.sendFile(path + '/index.html');
+router.get("/", function (req, res) {
+  res.sendFile(path + "/index.html");
+});
+router.get("/onoff", (req, res) => {
+  console.log(req.query.action);
+
+  client.publish("test", req.query.action, { qos: 0, retain: false }, (error) => {
+    if (error) {
+      console.error(error);
+    }
+  });
+  res.send("Get an existing workout");
 });
 
 // подключаем роутер к приложению
-app.use('/', router);
+app.use("/", router);
 
 // начинаем прослушивать порт 8080, тем самым запуская http-сервер
 app.listen(port, function () {
-    console.log('Listening on port 8080')
-})
+  console.log("Listening on port 8080");
+});
